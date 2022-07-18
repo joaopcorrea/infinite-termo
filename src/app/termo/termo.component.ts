995fb22f4d2a33c7354d68da80a28bb577477ec5
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-07-18 19:00:02 -0300

    feat: implementing the guess validation

--- src/app/termo/termo.component.ts
+++ src/app/termo/termo.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { WORDS } from '../words';
 
 // Length of the word
 const WORD_LENGTH = 5;
@@ -45,6 +46,7 @@ enum LetterState {
   styleUrls: ['./termo.component.scss']
 })
 export class TermoComponent {
+  @ViewChildren('tryContainer') tryContainers!: QueryList<ElementRef>;
   // Stores all tries
   // One try is one row in the UI
   readonly tries: Try[] = [];
@@ -52,11 +54,19 @@ export class TermoComponent {
   // Message shown in the message panel
   infoMsg = '';
 
+  // Controls info message's fading-out animation
+  fadeOutInfoMessage = false;
+
   // Tracks the current letter index
   private curLetterIndex = 0;
 
   // Tracks the number of submitted tries
   private numSubmittedTries = 0;
+
+  // Store the target word
+  private targetWord = '';
+
+  private targetWordLetterCounts: {[letter: string]: number} = {};
 
   constructor() {
     // Populate initial state of "tries"
@@ -67,6 +77,31 @@ export class TermoComponent {
       }
       this.tries.push({ letters });
     }
+
+    // Get a target word from the word list
+    const numWords = WORDS.length;
+    while(true) {
+      //Randomly select a word and check if its length is WORD_LENGTH
+      const index = Math.floor(Math.random() * numWords);
+      const word = WORDS[index];
+      if (word.length === WORD_LENGTH) {
+        this.targetWord = word.toLowerCase();
+        break;
+      }
+    }
+    console.log('Target word: ' + this.targetWord);
+
+    // Stores the count for each letter from the target word
+    // For example, if the target word is "happy", then this map will look like:
+    // { 'h':1, 'a':1, 'p':2, 'y':1, }
+    for (const letter of this.targetWord) {
+      const count = this.targetWordLetterCounts[letter];
+      if (count == null) {
+        this.targetWordLetterCounts[letter] = 0;
+      }
+      this.targetWordLetterCounts[letter]++;
+    }
+    console.log(this.targetWordLetterCounts);
   }
 
   @HostListener('document:keydown', ['$event'])
@@ -108,11 +143,59 @@ export class TermoComponent {
     // Check if user has typed all letters
     const curTry = this.tries[this.numSubmittedTries];
     if (curTry.letters.some(letter => letter.text === '')) {
-      console.log('not enough letters');
+      this.shakeCurrentRow();
+
+      if (curTry.letters.every(letter => letter.text === '')) {
+        return;
+      }
+      this.showInfoMessage('só palavras com 5 letras');
       return;
     }
+
+    // Check if the current try is a word in the list
+    const wordFromCurTry = 
+        curTry.letters.map(letter => letter.text).join('').toLowerCase();
+    if (!WORDS.includes(wordFromCurTry)) {
+      this.showInfoMessage('essa palavra não é aceita');
+
+      this.shakeCurrentRow();
+      return;
+    }
+
+    // Check if the current try matches the target word
+
+    // Store the check results
+    const states: LetterState[] = [];
+  }
+
+  private shakeCurrentRow() {
+    const tryContainer =
+      this.tryContainers.get(this.numSubmittedTries)
+        ?.nativeElement as HTMLElement;
+    tryContainer.classList.add('shake');
+    setTimeout(() => {
+      tryContainer.classList.remove('shake');
+    }, 500);
+  }
+
+  private showInfoMessage(msg: string) {
+    this.infoMsg = msg;
+    //Hide after 2s
+    setTimeout(() => {
+      this.fadeOutInfoMessage = true;
+      // Reset when animation is done
+      setTimeout(() => {
+        this.infoMsg = '';
+        this.fadeOutInfoMessage = false;
+      },  500);
+    }, 2000);
   }
   
+  isCurrentRow(t: Try, l: Letter) {
+    const tryIndex = this.tries.indexOf(t);
+    return this.numSubmittedTries >= tryIndex;
+  }
+
   isNextLetter(t: Try, l: Letter) {
     const tryIndex = this.tries.indexOf(t);
 
